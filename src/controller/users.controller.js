aa901e71f123308d4b328740f5e4e71a7112dafb
tankvivek-code <tankvivek65@gmail.com>
2025-08-06 16:03:53 +0530

    add in database

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,12 +1,23 @@
-export const getUsers = (req, res) => {
-  res.status(200).json([
-    {
-      id: 1,
-      Name: "Vivek",
-    },
-    {
-      id: 2,
-      Name: "Viral",
-    },
-  ]);
+import { user } from "../model/users.model.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const resposne = await user.find();
+    res.status(201).json({ users: resposne });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: error.message || "Error while getting user" });
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    await user.create({ username: "Vivek", password: "123" });
+    res.status(201).json({ message: "User created !" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: error.message || "Error while creating user" });
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
-import { getUsers } from "./controller/users.controller.js";
+import { createUser, getUsers } from "./controller/users.controller.js";
 import { getlanguages } from "./controller/language.controller.js";
+import { connectDB } from "./config/connectDB.js";
 
 const app = express();
 app.use(cors());
+connectDB();
 
 const PORT = 8000;
 
 app.get("/get-users", getUsers);
+app.post("/create-users", createUser);
 app.get("/get-language", getlanguages);
 
 app.listen(PORT, () => {
