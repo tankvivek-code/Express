e1662c0192c46c66a2d895a59003356d693c5e1a
tankvivek-code <tankvivek65@gmail.com>
2025-08-27 14:46:13 +0530

    fix: correct server running message format in index.js

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/lang", langRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
